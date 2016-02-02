2ac09a227db01b667ad66e8574b1c7222fba2cb0
Michael Sharp <msharp.oh@gmail.com>
2016-02-01 20:46:11 -0500

    added comments to dungeon-crawler

---
+++ fcc-dungeon-crawler/script.js
@@ -0,0 +1,27 @@
+/* Roguelike Dungeon Crawler
+    - This app is a web based roguelike dungeon crawler. The map will be randomly
+      generated and the user can explore, fight enemies, and pick up items. Based on
+      http://codepen.io/FreeCodeCamp/full/dGOOEJ/. This is a FreeCodeCamp project.
+
+      2016 Michael Sharp
+      www.softwareontheshore.com
+
+      USER STORIES:
+        - I have health, a level, and a weapon. I can pick up a better weapon. I can pick up health items.
+        - All the items and enemies on the map are arranged at random.
+        - I can move throughout a map, discovering items.
+        - I can move anywhere within the map's boundaries, but I can't move through an enemy until I've beaten it.
+        - Much of the map is hidden. When I take a step, all spaces that are within a certain number of spaces from me are revealed.
+        - When I beat an enemy, the enemy goes away and I get XP, which eventually increases my level.
+        - When I fight an enemy, we take turns damaging each other until one of us loses. I do damage based off of my level and my weapon. The enemy does damage based off of its level. Damage is somewhat random within a range.
+        - When I find and beat the boss, I win.
+        - The game should be challenging, but theoretically winnable.
+*/
+
+//user story: health, weapon, level
+var player = {
+  health: 100,
+  level: 1,
+  exp: 0,
+  weapon: { }
+};
