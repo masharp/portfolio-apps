101337713dddd818d0b0d8cb14607233a33a0627
Michael Sharp <msharp.oh@gmail.com>
2016-02-16 21:17:07 -0500

    finished file meta api service

--- shoreside-apis/file_metadata/README.md
+++ shoreside-apis/file_metadata/README.md
@@ -1,3 +1,4 @@
 #Shoreside File Metadata Microservice
   A web API that allows a user to submit a FormData file upload parameter with the request.
-    When the file is submitted, file metadata will be returned as JSON.
+  When the file is submitted, file metadata will be returned as JSON. Uses the [Multer](https://www.npmjs.com/package/multer)
+  npm package as a middleware to handle FormData file parsing.  
--- shoreside-apis/file_metadata/filemeta-api.js
+++ shoreside-apis/file_metadata/filemeta-api.js
@@ -1,2 +1,9 @@
 /* A web API that allows a user to submit a FormData file upload parameter with the request.
   When the file is submitted, file metadata will be returned as JSON. Hooks into an Express app.*/
+
+  router.post("/apis/filemeta", upload.single("file"), function(request, response, next) {
+    let responseObj = { "file_name" : request.file.originalname, "file_size_bytes" : request.file.size };
+
+    response.setHeader("Content-Type", "application/json");
+    response.json(responseObj);
+  });
